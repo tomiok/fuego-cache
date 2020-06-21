504ee3663d13ddda02192e96c497b3bc7f0ccc7d
tomaslingotti <tomaslingotti@gmail.com>
2020-06-21 20:59:45 -0300

    Add get method

--- safe/fuego/cache.go
+++ safe/fuego/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"errors"
 	"github.com/tomiok/fuego-cache/safe/encoding"
 	"github.com/tomiok/fuego-cache/safe/hash"
 	"sync"
@@ -12,6 +13,13 @@ type Cache struct {
 	Lock  sync.RWMutex
 }
 
+func NewCache() *Cache {
+	return &Cache{
+		Cache: &Fuego{},
+		Lock:  sync.RWMutex{},
+	}
+}
+
 //Fuego
 type Fuego struct {
 	Entries map[int][]byte
@@ -19,23 +27,35 @@ type Fuego struct {
 
 //FuegoEntry
 type Entry struct {
-	Key   int
-	Value []byte
+	Key        int
+	Value      []byte
+	Expiration int64
 }
 
-//AddOne will add an entry into the key-value store
+//AddOne will add an entry into the key-value store.
 func (c *Cache) AddOne(e Entry) bool {
 	c.Lock.Lock()
-	defer c.Lock.Unlock()
 	c.Cache.Entries[e.Key] = e.Value
+	c.Lock.Unlock()
 	return true
 }
 
+func (c *Cache) GetOne(key interface{}) interface{} {
+	c.Lock.RLock()
+	val := c.Cache.Entries[hash.Hash(key)]
+	c.Lock.RUnlock()
+	return val
+}
 
-func ToEntry(key, value interface{}) Entry {
+//ToEntry convert key value interfaces into a system Entry.
+func ToEntry(key, value interface{}) (Entry, error) {
 	encode := encoding.Encode(value)
+	if encode.Len() == 0 {
+		return Entry{}, errors.New("cannot encode the value")
+	}
+
 	return Entry{
 		Key:   hash.Hash(key),
 		Value: encode.Bytes(),
-	}
+	}, nil
 }
