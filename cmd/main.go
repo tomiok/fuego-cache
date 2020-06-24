6fb444db10de07b33d912f0b4f944bbdc19a8b7b
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2020-06-23 21:12:26 -0300

    feat(cmd): read env variable to start one of the servers

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/tomiok/fuego-cache/http_server"
+	"github.com/tomiok/fuego-cache/logs"
 	"github.com/tomiok/fuego-cache/tcp_server"
+	"os"
 )
 
 func main() {
+	mode := os.Getenv("MODE")
 
-	s := server.New("localhost:9919")
-	s.OnNewMessage(func(c *server.Client, message string) {
-		fmt.Println(message)
-	})
+	if mode == "tcp" {
+		s := server.New("localhost:9919")
+		s.OnNewMessage(func(c *server.Client, message string) {
+			fmt.Println(message)
+		})
 
-	s.Listen()
+		s.Listen()
+	} else if mode == "http" {
+		http := httpserver.NewHTTPServer("localhost:9919")
+		http.Listen()
+	} else {
+		logs.Fatal("Missing environment variable: MODE")
+	}
 }
