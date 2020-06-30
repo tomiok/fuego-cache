44d6cd3f7ac1fc759985f6022335b2b05bb61164
tomaslingotti <tomaslingotti@gmail.com>
2020-06-30 13:08:10 -0300

    add banner and stdio client

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	httpserver "github.com/tomiok/fuego-cache/http_server"
+	"github.com/tomiok/fuego-cache/internal"
 	"github.com/tomiok/fuego-cache/logs"
 	cache "github.com/tomiok/fuego-cache/safe/fuego"
+	"os/signal"
 
 	"github.com/tomiok/fuego-cache/tcp_server"
 	"os"
 )
 
 func main() {
+	printBanner()
 	mode := os.Getenv("MODE")
 	var fuegoInstance = cache.NewCache()
 	if mode == "tcp" {
@@ -28,6 +32,29 @@ func main() {
 		http := httpserver.NewHTTPServer("localhost:9919")
 		http.Listen()
 	} else {
-		logs.Fatal("Missing environment variable: MODE")
+		standardInputCache()
 	}
+}
+
+func standardInputCache() {
+	quit := make(chan os.Signal, 1)
+	go func() {
+		for {
+			logs.Info("start with fuego here... (add 1 1)")
+			reader := bufio.NewReader(os.Stdin)
+			text, _ := reader.ReadString('\n')
+			logs.Info(text)
+		}
+	}()
+
+	signal.Notify(quit, os.Interrupt)
+
+	select {
+	case <-quit:
+		logs.Info("exiting...")
+	}
+}
+
+func printBanner() {
+	internal.PrintBanner()
 }
