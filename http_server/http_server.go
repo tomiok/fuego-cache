226524608367de86a09edd45997df05d0b3db95d
tomaslingotti <tomaslingotti@gmail.com>
2020-07-21 18:33:18 -0300

    add gracefull shutdown

--- http_server/http_server.go
+++ http_server/http_server.go
@@ -1,8 +1,12 @@
 package httpServer
 
 import (
+	"context"
 	"github.com/tomiok/fuego-cache/logs"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 type FuegoHTTPServer struct {
@@ -10,7 +14,12 @@ type FuegoHTTPServer struct {
 }
 
 func (f *FuegoHTTPServer) Listen() {
-	logs.Fatal(f.server.ListenAndServe())
+	go func() {
+		if err := f.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logs.Fatal("could not listen " + f.server.Addr + " due to: " + err.Error())
+		}
+	}()
+	f.gracefulShutdown()
 }
 
 func NewHTTPServer(address string, mux http.Handler) *FuegoHTTPServer {
@@ -22,4 +31,21 @@ func NewHTTPServer(address string, mux http.Handler) *FuegoHTTPServer {
 	return &FuegoHTTPServer{
 		server: server,
 	}
+}
+
+func (f *FuegoHTTPServer) gracefulShutdown() {
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt)
+	sig := <-quit
+	logs.Info("server is shutting down " + sig.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	f.server.SetKeepAlivesEnabled(false)
+	if err := f.server.Shutdown(ctx); err != nil {
+		logs.Fatal("could not gracefully shutdown the server " + err.Error())
+	}
+	logs.Info("server stopped")
 }
