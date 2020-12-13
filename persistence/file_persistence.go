4f04fae0b4a69f565074bfe05c72a4bded8e3e6a
tomas lingotti <tomaslingotti@gmail.com>
2020-12-13 04:41:51 -0300

    WIP

--- persistence/file_persistence.go
+++ persistence/file_persistence.go
@@ -1,16 +1,22 @@
 package persistence
 
 import (
+	"errors"
 	"fmt"
+	cache "github.com/tomiok/fuego-cache/fuego"
 	"github.com/tomiok/fuego-cache/internal"
 	"github.com/tomiok/fuego-cache/logs"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type Persist interface {
 	Save(operation string, k int, value string)
+	Get(key string) (string, error)
 }
 
 type Data struct {
@@ -20,7 +26,7 @@ type Data struct {
 }
 
 type FilePersistence struct {
-	File string
+	File     string
 	InMemory bool
 }
 
@@ -44,6 +50,36 @@ func (f *FilePersistence) Save(operation string, k int, value string) {
 		logs.LogError(err)
 		// no error returned, just a shame
 	}
+}
+
+func (f *FilePersistence) Get(key string) (string, error) {
+	bytes, err := ioutil.ReadFile(f.File)
+
+	if err != nil {
+		return "", err
+	}
+
+	text := string(bytes)
+
+	pairs := strings.Split(text, "\n")
+
+	for _, kv := range pairs {
+		values := strings.Split(kv, ",")
+		hashedKey := values[0]
+		i, err := strconv.Atoi(hashedKey)
+
+		if err != nil {
+			logs.Error("cannot parse key into INT type. " + err.Error())
+			return "", nil
+		}
+
+		if i == cache.ApplyHash(key) {
+			return values[1], nil
+		}
+	}
+
+	return "", errors.New("key not fiound")
+
 }
 
 func buildRecord(operation string, k int, value string, inMemory bool) string {
